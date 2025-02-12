a631d1c4b5c47f11e4b0a715366664d7293f95df
Fausto González Castro <fau.gc90@gmail.com>
2025-02-12 18:27:42 -0300

    Update database.js

--- src/components/database.js
+++ src/components/database.js
@@ -1,9 +1,7 @@
-// Importa las funciones necesarias de Firebase
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc, updateDoc, addDoc } from "firebase/firestore";  // Elimina la segunda importación de 'collection'
-import productosimg from './info';  // Asegúrate de que este path sea correcto
+import { getFirestore, collection, getDocs, doc, updateDoc, addDoc } from "firebase/firestore"; 
+import productosimg from './info';  
 
-// Configuración de Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAQSLbdTyHuUATFquCx55rO7KmLPGUPs5k",
     authDomain: "lembranzas-cf087.firebaseapp.com",
@@ -13,27 +11,22 @@ const firebaseConfig = {
     appId: "1:253626952493:web:c24d3b2e5b7c5fae4641a4"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Aquí es donde obtenemos la instancia de Firestore
+const db = getFirestore(app); 
 
-// Exportamos la variable db para poder usarla en otros archivos
 export { db };
 
-// Funciones existentes de Firebase y productos
 export default async function getAsyncData() {
     const collectionRef = collection(db, "productos");
     const productosSnapshot = await getDocs(collectionRef);
     console.log(productosSnapshot);
 
-    // Obtiene los productos de Firebase
     const documentsData = productosSnapshot.docs.map(doc => {
         const fullData = doc.data()
         fullData.id = doc.id;
         return fullData;
     });
 
-    // Asocia los productos con las imágenes de info.js utilizando el id
     const productsWithImages = documentsData.map(product => {
         const productImages = productosimg.find(item => item.id === parseInt(product.id)); 
         if (productImages) {
@@ -45,19 +38,16 @@ export default async function getAsyncData() {
     return productsWithImages;
 }
 
-// Obtener un producto por ID
 export async function getAsyncItemById(itemID) {
     const productos = await getAsyncData();
     return productos.find(item => item.id === itemID) || null;
 }
 
-// Obtener productos por categoría
 export async function getAsyncItemByCategory(catID) {
     const productos = await getAsyncData();
     return productos.filter(item => item.categoria === catID);
 }
 
-// Función para crear una orden de compra
 export async function createBuyOrder (orderData){   
     const newOrderDoc = await addDoc(collection(db, "orders"), orderData);
     return newOrderDoc.id;
