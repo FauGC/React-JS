6b65dd0ae7d58bd5baf11af51d61b68ac2a7946e
Fausto González Castro <fau.gc90@gmail.com>
2025-02-12 18:00:45 -0300

    Update CartWidget.jsx

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { createBuyOrder } from './database'; // Se importa createBuyOrder desde database.js
+import { createBuyOrder } from './database';
 
 const CartWidget = ({ cantidad, cart, setCart }) => {
     const [isCarritoVisible, setIsCarritoVisible] = useState(false);
@@ -11,7 +11,7 @@ const CartWidget = ({ cantidad, cart, setCart }) => {
         email: "",
         phone: ""
     });
-    const [purchaseCompleted, setPurchaseCompleted] = useState(false); // Estado para saber si la compra fue realizada
+    const [purchaseCompleted, setPurchaseCompleted] = useState(false); 
 
     useEffect(() => {
         const savedCart = localStorage.getItem('cartItems');
@@ -101,7 +101,7 @@ const CartWidget = ({ cantidad, cart, setCart }) => {
         createBuyOrder(orderData)
             .then(newOrderId => {
                 console.log('Compra realizada con éxito, orden ID:', newOrderId, orderData);
-                setOrderId(newOrderId); // Guardar el ID de la orden
+                setOrderId(newOrderId); 
             })
             .catch(error => {
                 console.error("Error al crear la orden:", error);
@@ -112,9 +112,9 @@ const CartWidget = ({ cantidad, cart, setCart }) => {
         onSubmit();
         vaciarCarrito();
         localStorage.clear();
-        setUserData({ username: "", surname: "", age: "", email: "", phone: "" }); // Limpiar datos del formulario
+        setUserData({ username: "", surname: "", age: "", email: "", phone: "" }); 
         setPurchaseCompleted(true); 
-        setIsCarritoVisible(false); // Cerrar el carrito después de realizar la compra
+        setIsCarritoVisible(false); 
     };
 
     const isFormComplete = userData.username && userData.surname && userData.age && userData.email && userData.phone;
@@ -132,8 +132,8 @@ const CartWidget = ({ cantidad, cart, setCart }) => {
                         {cart.length > 0 ? (
                             cart.map(producto => {
                                 const imageUrl = producto.imagenes && producto.imagenes[0]
-                                    ? `./imagenes/${producto.imagenes[0]}`
-                                    : './imagenes/default.jpg';
+                                    ? `${producto.imagenes[0]}`
+                                    : 'default.jpg';
                                 return (
                                     <div key={producto.id} className="producto-en-carrito">
                                         <div className="producto-imagen">
